8e33d98fcf2b17784ccdb67d0a334b8ea040f0bb
Pedro Junqueira <pedrocj@gmail.com>
2020-10-03 16:59:52 +0930

    add mockey patch and test summarizer

--- project/tests/test_summaries.py
+++ project/tests/test_summaries.py
@@ -6,15 +6,6 @@ import json
 import pytest
 
 
-def test_create_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
-
-    assert response.status_code == 201
-    assert response.json()["url"] == "https://foo.bar"
-
-
 def test_create_summaries_invalid_json(test_app):
     response = test_app.post("/summaries/", data=json.dumps({}))
     assert response.status_code == 422
@@ -31,22 +22,6 @@ def test_create_summaries_invalid_json(test_app):
     response = test_app.post("/summaries/", data=json.dumps({"url": "invalid://url"}))
     assert response.status_code == 422
     assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
-
-
-def test_read_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.get(f"/summaries/{summary_id}/")
-    assert response.status_code == 200
-
-    response_dict = response.json()
-    assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
-    assert response_dict["summary"]
-    assert response_dict["created_at"]
 
 
 def test_read_summary_incorrect_id(test_app_with_db):
@@ -68,30 +43,6 @@ def test_read_summary_incorrect_id(test_app_with_db):
     }
 
 
-def test_read_all_summaries(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.get("/summaries/")
-    assert response.status_code == 200
-
-    response_list = response.json()
-    assert len(list(filter(lambda d: d["id"] == summary_id, response_list))) == 1
-
-
-def test_remove_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.delete(f"/summaries/{summary_id}/")
-    assert response.status_code == 200
-    assert response.json() == {"id": summary_id, "url": "https://foo.bar"}
-
-
 def test_remove_summary_incorrect_id(test_app_with_db):
     response = test_app_with_db.delete("/summaries/999/")
     assert response.status_code == 404
@@ -109,25 +60,6 @@ def test_remove_summary_incorrect_id(test_app_with_db):
             }
         ]
     }
-
-
-def test_update_summary(test_app_with_db):
-    response = test_app_with_db.post(
-        "/summaries/", data=json.dumps({"url": "https://foo.bar"})
-    )
-    summary_id = response.json()["id"]
-
-    response = test_app_with_db.put(
-        f"/summaries/{summary_id}/",
-        data=json.dumps({"url": "https://foo.bar", "summary": "updated!"}),
-    )
-    assert response.status_code == 200
-
-    response_dict = response.json()
-    assert response_dict["id"] == summary_id
-    assert response_dict["url"] == "https://foo.bar"
-    assert response_dict["summary"] == "updated!"
-    assert response_dict["created_at"]
 
 
 @pytest.mark.parametrize(
